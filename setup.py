c4a95aae747e6ab563f99416de545d85e6e29284
NotTheEvilOne <git-NTEO@vplace.de>
2016-11-17 16:47:51 +0100

    Trivial: Fixed PEP 8 inconsistent indentation

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-##j## BOF
 
 """
 direct PAS
@@ -18,15 +17,14 @@ setup.py
 """
 
 def get_version():
-#
-	"""
+    """
 Returns the version currently in development.
 
 :return: (str) Version string
 :since:  v0.1.00
-	"""
+    """
 
-	return "v0.2.00"
+    return "v0.2.00"
 #
 
 from dNG.distutils.command.build_py import BuildPy
@@ -38,40 +36,37 @@ from distutils.core import setup
 from os import path
 
 with TemporaryDirectory(dir = ".") as build_directory:
+    js_copyright = "pas.http.dom_editor #echo(pasHttpDomEditorVersion)# - (C) direct Netware Group - All rights reserved"
+
+    parameters = { "pasHttpDomEditorVersion": get_version(),
+                   "js_header": js_copyright, "js_min_filenames": True
+                 }
+
+    InstallData.add_install_data_callback(InstallJsData.callback, [ "data" ])
+    InstallData.set_build_target_path(build_directory)
+    InstallData.set_build_target_parameters(parameters)
+
+    _build_path = path.join(build_directory, "src")
+
+    setup(name = "pas_http_dom_editor",
+          version = get_version(),
+          description = "Python Application Services",
+          long_description = """"pas_http_dom_editor" provides an easy API for client DOM tree manipulation.""",
+          author = "direct Netware Group et al.",
+          author_email = "web@direct-netware.de",
+          license = "MPL2",
+          url = "https://www.direct-netware.de/redirect?pas;http;dom_editor",
+
+          platforms = [ "any" ],
+
+          package_dir = { "": _build_path },
+          packages = [ "dNG" ],
+
+          data_files = [ ( "docs", [ "LICENSE", "README" ]) ],
+
+          # Override build_py to first run builder.py over all PAS modules
+          cmdclass = { "build_py": BuildPy,
+                       "install_data": InstallData
+                     }
+         )
 #
-	js_copyright = "pas.http.dom_editor #echo(pasHttpDomEditorVersion)# - (C) direct Netware Group - All rights reserved"
-
-	parameters = { "pasHttpDomEditorVersion": get_version(),
-	               "js_header": js_copyright, "js_min_filenames": True
-	             }
-
-	InstallData.add_install_data_callback(InstallJsData.callback, [ "data" ])
-	InstallData.set_build_target_path(build_directory)
-	InstallData.set_build_target_parameters(parameters)
-
-	_build_path = path.join(build_directory, "src")
-
-	setup(name = "pas_http_dom_editor",
-	      version = get_version(),
-	      description = "Python Application Services",
-	      long_description = """"pas_http_dom_editor" provides an easy API for client DOM tree manipulation.""",
-	      author = "direct Netware Group et al.",
-	      author_email = "web@direct-netware.de",
-	      license = "MPL2",
-	      url = "https://www.direct-netware.de/redirect?pas;http;dom_editor",
-
-	      platforms = [ "any" ],
-
-	      package_dir = { "": _build_path },
-	      packages = [ "dNG" ],
-
-	      data_files = [ ( "docs", [ "LICENSE", "README" ]) ],
-
-	      # Override build_py to first run builder.py over all PAS modules
-	      cmdclass = { "build_py": BuildPy,
-	                   "install_data": InstallData
-	                 }
-	)
-#
-
-##j## EOF
--- src/dNG/module/controller/services/abstract_dom_editor.py
+++ src/dNG/module/controller/services/abstract_dom_editor.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-##j## BOF
 
 """
 direct PAS
@@ -25,8 +24,7 @@ from dNG.data.xhtml.oset.file_parser import FileParser
 from .module import Module
 
 class AbstractDomEditor(Module):
-#
-	"""
+    """
 "DomEditor" implementations are used to handle API calls that might change
 the client DOM tree.
 
@@ -37,42 +35,39 @@ the client DOM tree.
 :since:      v0.2.00
 :license:    https://www.direct-netware.de/redirect?licenses;mpl2
              Mozilla Public License, v. 2.0
-	"""
+    """
 
-	def __init__(self):
-	#
-		"""
+    def __init__(self):
+        """
 Constructor __init__(AbstractDomEditor)
 
 :since: v0.2.00
-		"""
+        """
 
-		Module.__init__(self)
+        Module.__init__(self)
 
-		self.dom_id = None
-		"""
+        self.dom_id = None
+        """
 ID of the client DOM tree to manipulate.
-		"""
-	#
+        """
+    #
 
-	def _check_response_instance_supported(self):
-	#
-		"""
+    def _check_response_instance_supported(self):
+        """
 Checks if the response instance supports returning API result dicts.
 
 :return: (bool) True if supported
 :since:  v0.2.00
-		"""
+        """
 
-		return (False
-		        if (not isinstance(self.response, AbstractResponse)) else
-		        self.response.is_supported("dict_result_renderer")
-		       )
-	#
+        return (False
+                if (not isinstance(self.response, AbstractResponse)) else
+                self.response.is_supported("dict_result_renderer")
+               )
+    #
 
-	def _set_append_dom_oset_result(self, template_name, content, **kwargs):
-	#
-		"""
+    def _set_append_dom_oset_result(self, template_name, content, **kwargs):
+        """
 Returns and activates the client API "append_dom" after rendering the given
 content with the OSet template. "append_dom_id" is used in case the client
 target DOM ID does not correspond to the one calling the server API.
@@ -81,18 +76,17 @@ target DOM ID does not correspond to the one calling the server API.
 :param content: Content object
 
 :since: v0.2.00
-		"""
+        """
 
-		oset = self.request.get_dsd("doset")
+        oset = self.request.get_dsd("doset")
 
-		parser = FileParser()
-		if (oset is not None): parser.set_oset(oset)
-		self._set_append_dom_result(parser.render(template_name, content), **kwargs)
-	#
+        parser = FileParser()
+        if (oset is not None): parser.set_oset(oset)
+        self._set_append_dom_result(parser.render(template_name, content), **kwargs)
+    #
 
-	def _set_append_dom_result(self, dom_value, **kwargs):
-	#
-		"""
+    def _set_append_dom_result(self, dom_value, **kwargs):
+        """
 Returns the DOM value given and activates the client API "append_dom".
 "append_dom_id" is used in case the client target DOM ID does not correspond
 to the one calling the server API.
@@ -100,54 +94,51 @@ to the one calling the server API.
 :param dom_value: DOM value to response
 
 :since: v0.2.00
-		"""
+        """
 
-		api_call = "append_dom"
-		dom_id = self.dom_id
+        api_call = "append_dom"
+        dom_id = self.dom_id
 
-		if (dom_id is None): dom_id = InputFilter.filter_file_path(self.request.get_dsd("ddom_id", ""))
-		elif (self.request.get_dsd("ddom_id", "") != dom_id): api_call = "append_dom_id"
+        if (dom_id is None): dom_id = InputFilter.filter_file_path(self.request.get_dsd("ddom_id", ""))
+        elif (self.request.get_dsd("ddom_id", "") != dom_id): api_call = "append_dom_id"
 
-		result = kwargs
+        result = kwargs
 
-		result.update({ "api_call": api_call,
-		                "dom_id": dom_id,
-		                "dom_value": dom_value
-		              })
+        result.update({ "api_call": api_call,
+                        "dom_id": dom_id,
+                        "dom_value": dom_value
+                      })
 
-		self.response.set_result(result)
-	#
+        self.response.set_result(result)
+    #
 
-	def _set_destroy_dom_result(self, **kwargs):
-	#
-		"""
+    def _set_destroy_dom_result(self, **kwargs):
+        """
 Activates the client API "destroy_dom".
 
 :since: v0.2.00
-		"""
+        """
 
-		result = kwargs
-		result.update({ "api_call": "destroy_dom" })
+        result = kwargs
+        result.update({ "api_call": "destroy_dom" })
 
-		self.response.set_result(result)
-	#
+        self.response.set_result(result)
+    #
 
-	def _set_dom_id(self, dom_id):
-	#
-		"""
+    def _set_dom_id(self, dom_id):
+        """
 Sets the ID of the client DOM tree to manipulate.
 
 :param dom_id: ID of the client DOM tree
 
 :since: v0.2.00
-		"""
+        """
 
-		self.dom_id = dom_id
-	#
+        self.dom_id = dom_id
+    #
 
-	def _set_append_overlay_dom_oset_result(self, template_name, content, **kwargs):
-	#
-		"""
+    def _set_append_overlay_dom_oset_result(self, template_name, content, **kwargs):
+        """
 Returns and activates the client API "append_overlay_dom" after rendering
 the given content with the OSet template. "append_overlay_dom_id" is used in
 case the client target DOM ID does not correspond to the one calling the
@@ -157,18 +148,17 @@ server API.
 :param content: Content object
 
 :since: v0.2.00
-		"""
+        """
 
-		oset = self.request.get_dsd("doset")
+        oset = self.request.get_dsd("doset")
 
-		parser = FileParser()
-		if (oset is not None): parser.set_oset(oset)
-		self._set_append_overlay_dom_result(parser.render(template_name, content), **kwargs)
-	#
+        parser = FileParser()
+        if (oset is not None): parser.set_oset(oset)
+        self._set_append_overlay_dom_result(parser.render(template_name, content), **kwargs)
+    #
 
-	def _set_append_overlay_dom_result(self, dom_value, **kwargs):
-	#
-		"""
+    def _set_append_overlay_dom_result(self, dom_value, **kwargs):
+        """
 Returns the DOM value given and activates the client API
 "append_overlay_dom". "append_overlay_dom_id" is used in case the client
 target DOM ID does not correspond to the one calling the server API.
@@ -176,27 +166,26 @@ target DOM ID does not correspond to the one calling the server API.
 :param dom_value: DOM value to response
 
 :since: v0.2.00
-		"""
+        """
 
-		api_call = "append_overlay_dom"
-		dom_id = self.dom_id
+        api_call = "append_overlay_dom"
+        dom_id = self.dom_id
 
-		if (dom_id is None): dom_id = InputFilter.filter_file_path(self.request.get_dsd("ddom_id", ""))
-		elif (self.request.get_dsd("ddom_id", "") != dom_id): api_call = "append_overlay_dom_id"
+        if (dom_id is None): dom_id = InputFilter.filter_file_path(self.request.get_dsd("ddom_id", ""))
+        elif (self.request.get_dsd("ddom_id", "") != dom_id): api_call = "append_overlay_dom_id"
 
-		result = kwargs
+        result = kwargs
 
-		result.update({ "api_call": api_call,
-		                "dom_id": dom_id,
-		                "dom_value": dom_value
-		              })
+        result.update({ "api_call": api_call,
+                        "dom_id": dom_id,
+                        "dom_value": dom_value
+                      })
 
-		self.response.set_result(result)
-	#
+        self.response.set_result(result)
+    #
 
-	def _set_replace_dom_oset_result(self, template_name, content, **kwargs):
-	#
-		"""
+    def _set_replace_dom_oset_result(self, template_name, content, **kwargs):
+        """
 Returns and activates the client API "replace_dom" after rendering the given
 content with the OSet template. "replace_dom_id" is used in case the client
 target DOM ID does not correspond to the one calling the server API.
@@ -205,18 +194,17 @@ target DOM ID does not correspond to the one calling the server API.
 :param content: Content object
 
 :since: v0.2.00
-		"""
+        """
 
-		oset = self.request.get_dsd("doset")
+        oset = self.request.get_dsd("doset")
 
-		parser = FileParser()
-		if (oset is not None): parser.set_oset(oset)
-		self._set_replace_dom_result(parser.render(template_name, content), **kwargs)
-	#
+        parser = FileParser()
+        if (oset is not None): parser.set_oset(oset)
+        self._set_replace_dom_result(parser.render(template_name, content), **kwargs)
+    #
 
-	def _set_replace_dom_result(self, dom_value, **kwargs):
-	#
-		"""
+    def _set_replace_dom_result(self, dom_value, **kwargs):
+        """
 Returns the DOM value given and activates the client API "replace_dom".
 "replace_dom_id" is used in case the client target DOM ID does not
 correspond to the one calling the server API.
@@ -224,23 +212,21 @@ correspond to the one calling the server API.
 :param dom_value: DOM value to response
 
 :since: v0.2.00
-		"""
+        """
 
-		api_call = "replace_dom"
-		dom_id = self.dom_id
+        api_call = "replace_dom"
+        dom_id = self.dom_id
 
-		if (dom_id is None): dom_id = InputFilter.filter_file_path(self.request.get_dsd("ddom_id", ""))
-		elif (self.request.get_dsd("ddom_id", "") != dom_id): api_call = "replace_dom_id"
+        if (dom_id is None): dom_id = InputFilter.filter_file_path(self.request.get_dsd("ddom_id", ""))
+        elif (self.request.get_dsd("ddom_id", "") != dom_id): api_call = "replace_dom_id"
 
-		result = kwargs
+        result = kwargs
 
-		result.update({ "api_call": api_call,
-		                "dom_id": dom_id,
-		                "dom_value": dom_value
-		              })
+        result.update({ "api_call": api_call,
+                        "dom_id": dom_id,
+                        "dom_value": dom_value
+                      })
 
-		self.response.set_result(result)
-	#
+        self.response.set_result(result)
+    #
 #
-
-##j## EOF
